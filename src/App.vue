d990dd61dc431086c779574416b0a0157fb2fe9f
JUDPro <vitaliyvlsdm@gmail.com>
2021-07-27 22:19:50 +0700

    some fix

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <div>{{ start }}</div>
+  <v-app>
+    
+  </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  data() {
-    return {
-      start: "Hi",
-    };
-  },
-});
+import Vue from 'vue'
+export default Vue.extend({})
 </script>
-
-<style></style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-    entry: './src/main.ts',
-    module: {
-      rules: [
-        { test: /\.js$/, use: 'babel-loader' },
-        { test: /\.vue$/, use: 'vue-loader' },
-        { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
-      ]
-    },
-    devtool: "source-map",
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      new VueLoaderPlugin(),
-    ]
-  };
+  entry: "./src/main.ts",
+  module: {
+    rules: [
+      { test: /\.js$/, use: "babel-loader" },
+      { test: /\.vue$/, use: "vue-loader" },
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              indentedSyntax: true,
+            },
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                indentedSyntax: true,
+              },
+            },
+          },
+        ],
+      },
+      { test: /\.css$/, use: ["vue-style-loader", "css-loader"] },
+    ],
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
