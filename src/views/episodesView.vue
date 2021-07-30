3ee99c1d9976d89051eec94d1cd978e652e2bd7a
JUDPro <vitaliyvlsdm@gmail.com>
2021-07-30 23:26:32 +0700

    Working search function

--- src/views/episodesView.vue
+++ src/views/episodesView.vue
@@ -2,30 +2,42 @@
   <div>
     <v-card-actions class="d-flex justify-space-between mx-10 px-5">
       <div>Episode</div>
-      <div v-if="!isMobile">
-        <div class="text-center">
-          <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                Season
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                style="cursor: pointer"
-                class="d-flex justify-center"
-                v-for="item in seasonsList"
-                :key="item.index"
-                @click="groupSeasons(item.season)"
-              >
-                <v-btn-toggle>{{ item.season }}</v-btn-toggle>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
+
+      <div class="text-center" v-if="!isMobile">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              Season
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              style="cursor: pointer"
+              class="d-flex justify-center"
+              v-for="item in seasonsList"
+              :key="item.index"
+              @click="groupSeasons(item.season)"
+            >
+              <v-btn-toggle>{{ item.season }}</v-btn-toggle>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </div>
+
+      <div class="d-flex align-center">
+        <v-text-field
+          class="mr-5"
+          v-model="nameEpisode"
+          label="Search"
+          clearable
+          @keyup.enter="episodeSearchResults"
+        ></v-text-field>
+        <v-btn elevation="2" @click="episodeSearchResults">Enter</v-btn>
+      </div>
+
       <div v-if="!isMobile">Date</div>
     </v-card-actions>
+
     <episodeItem v-for="item in episodeList" :key="item.index">
       <span
         slot="episodeName"
@@ -68,12 +80,13 @@ export default Vue.extend({
         { season: "4" },
       ],
       season: "1" as String,
+      nameEpisode: "" as String,
     };
   },
   methods: {
-    getData(item: String) {
+    getData(season: String) {
       axios
-        .get("https://rickandmortyapi.com/api/episode/?episode=S0" + item)
+        .get("https://rickandmortyapi.com/api/episode/?episode=S0" + season)
         .then((response) => {
           const result = response.data.results;
           this.episodeList = result;
@@ -86,6 +99,26 @@ export default Vue.extend({
     },
     groupSeasons(list: String) {
       this.getData(list);
+    },
+    episodeSearchResults() {
+      axios
+        .get(
+          "https://rickandmortyapi.com/api/episode/?name=" +
+            this.nameEpisode.toLowerCase()
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            const result = response.data.results;
+            this.episodeList = result;
+          }
+        })
+        .catch((err) => {
+          this.episodeList = [{
+            name: "Nothing was found :(",
+            episode: "",
+            air_date: "",
+          }];
+        });
     },
   },
   computed: {
