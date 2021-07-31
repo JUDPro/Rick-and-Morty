115d1a9ea1b3195a5351e8de05a112e555598bb0
JUDPro <vitaliyvlsdm@gmail.com>
2021-07-31 13:50:17 +0700

    added filter by status

--- src/views/CharactersList.vue
+++ src/views/CharactersList.vue
@@ -1,17 +1,53 @@
 <template>
-  <v-row>
-    <v-col
-      class="col-md-3"
-      v-for="item in charactersList"
-      :key="item.index"
-      @click="goToCharacterData(item)"
-    >
-      <card-of-character :urlImg="item.image" :nameCharacter="item.name">
-        <span slot="statusCharacter">{{ item.status }}</span>
-        <span slot="speciesCharacter">{{ item.species }}</span>
-      </card-of-character>
-    </v-col>
-  </v-row>
+  <div>
+    <div class="d-flex justify-center">
+      <div class="text-center" flat>
+        <div class="d-flex align-center" style="max-width: 300px;">
+          <v-text-field
+            class="mr-5"
+            v-model="characterName"
+            label="Search"
+            clearable
+            @keyup.enter="characterSearchResults"
+          ></v-text-field>
+          <v-btn elevation="2" @click="characterSearchResults">Enter</v-btn>
+        </div>
+        <div class="ma-5">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Filter by status
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                style="cursor: pointer"
+                class="d-flex justify-center"
+                v-for="item in statusList"
+                :key="item.index"
+                @click="filterByStatus(item.status)"
+              >
+                <v-btn-toggle>{{ item.status }}</v-btn-toggle>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </div>
+    </div>
+    <v-row>
+      <v-col
+        class="col-md-3"
+        v-for="item in charactersList"
+        :key="item.id"
+        @click="goToCharacterData(item)"
+      >
+        <card-of-character :urlImg="item.image" :nameCharacter="item.name">
+          <span slot="statusCharacter">{{ item.status }}</span>
+          <span slot="speciesCharacter">{{ item.species }}</span>
+        </card-of-character>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,20 +61,55 @@ export default Vue.extend({
   data() {
     return {
       charactersList: {},
+      characterName: "",
+      statusList: [
+        { status: "alive" },
+        { status: "dead" },
+        { status: "unknow" },
+      ],
     };
   },
   methods: {
+    getData(item: any) {
+      axios
+        .get("https://rickandmortyapi.com/api/character/?status=" + item)
+        .then((response) => {
+          this.charactersList = response.data.results;
+        });
+    },
     goToCharacterData(item: any) {
       this.$router
-        .push({ path: "/character/" + item.id, params: item.id
-        })
+        .push({ path: "/character/" + item.id, params: item.id })
         .catch(() => {});
+    },
+    characterSearchResults() {
+      axios
+        .get(
+          "https://rickandmortyapi.com/api/character/?name=" +
+            this.characterName.toLowerCase()
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            const result = response.data.results;
+            this.charactersList = result;
+          }
+        })
+        .catch((err) => {
+          this.charactersList = [
+            {
+              name: "Nothing was found :(",
+              episode: "",
+              air_date: "",
+            },
+          ];
+        });
+    },
+    filterByStatus(list: String) {
+      this.getData(list);
     },
   },
   mounted() {
-    axios.get("https://rickandmortyapi.com/api/character").then((response) => {
-      this.charactersList = response.data.results;
-    });
+    this.getData("");
   },
 });
 </script>
